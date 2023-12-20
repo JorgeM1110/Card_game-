feb10a98cab409adc30f940d29b7520c946f9f61
Classy-Brian <phanbrian101@gmail.com>
2023-12-20 13:39:23 -0800

    card documentation

--- card.py
+++ card.py
@@ -5,11 +5,12 @@ class Card:
         <<get>> _hp (int): health of the card
         _max_hp (int): max health of the card
         _cost (int): cost of the card
-        _power (int): power of the card
+        <<get>> _power (int): power of the card
         _sigil (string): sigil of the card
     """
 
     def __init__(self, name, cost, power, max_health):
+        """ Initializes attributes """
         self._name = name
         self._cost = cost
         self._power = power
@@ -18,17 +19,21 @@ class Card:
 
     @property
     def name(self):
+        """ Getter for name """
         return self._name
 
     @property
     def hp(self):
+        """ Getter for health """
         return self._hp
 
     @property
     def power(self):
+        """ Getter for power """
         return self._power
 
     def take_damage(self, dmg):
+        """ Takes damge, subtracts health from damage """
         if self._hp > dmg:
             self._hp -= dmg
         else:
@@ -36,8 +41,10 @@ class Card:
         return f"{self.name} takes {dmg} damage"
 
     def attack(self, entity, dmg):
+        """ Deals damage to opposing entity """
         entity.take_damage(dmg)
         return f"{entity.name} delts {dmg} to {self.name}"
 
     def __str__(self):
+        """ returns name, health, and sigil """
         return f"{self.name} \n{self._hp}/{self._max_health} \nSigil: {self._sigil}"
