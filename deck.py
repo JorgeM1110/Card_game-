2d895ce86a62ab7a9326d8d08a3d22454392d91b
Jorge <@mejia>
2023-12-20 21:46:11 -0800

    deck update

--- deck.py
+++ deck.py
@@ -1,6 +1,7 @@
 import card 
 import random
 
+
 class Deck:
 
     def __init__(self):
@@ -30,44 +31,93 @@ class Deck:
         cards = random.randint(1,2)
         if cards == 1:
             card._power += 1
-            print("Your power has upgrade to 1\n")
+            print("Your power has upgrade to " + card._power + "\n")
         else:
             card._max_health += 1
-            print("Your max health has upgrade to 1\n")
+            print("Your max health has upgrade to " + card._max_health + "\n")
         
         player_choice = input(f" would you like to upgrade again? 50% change\n Y/N")
         chance = 50
 
-        while player_choice == 'Y':
+        while player_choice == 'Y' and chance > 0 :
+            random_num = random.randint(1,100)
+            if chance == 50:
+                if random_num <= 50:
+                    print("Well done you have Luck!")
+                    if random_num <= 25:
+                        card._power += 1
+                        print("Your power has upgrade to " + card._power + "\n")
+                    else:
+                        card._max_health += 2
+                        print("Your max health has upgrade to " + card._max_health + "\n")
+                else:
+                    card.pop(0)
+                    print("Better luck next time")
 
-            cards = random.randint(1,2)
-            if cards == 1:
-                print(hello)
+            elif chance == 25:
+                if random_num <= 25:
+                    print("Well done you have Luck Again!")
+                    if random_num <= 12:
+                        card._power += 1
+                        print("Your power has upgrade to " + card._power + "\n")
+                    else:
+                        card._max_health += 2
+                        print("Your max health has upgrade to " + card._max_health + "\n")
+                else:
+                    card.pop(0)
+                    print("Your Luck runs out")
 
+            elif chance == 12:
+                if random_num <= 12:
+                    print("You're on Fire!!")
+                    if random_num <= 6:
+                        card._power += 1
+                        print("Your power has upgrade to " + card._power + "\n")
+                    else:
+                        card._max_health += 2
+                        print("Your max health has upgrade to " + card._max_health + "\n")
+                else:
+                    card.pop(0)
+                    print("You got too Greedy")
 
+            elif chance == 6:
+                if random_num <= 6:
+                    print("You're Crazy!!")
+                    if random_num <= 3:
+                        card._power += 1
+                        print("Your power has upgrade to " + card._power + "\n")
+                    else:
+                        card._max_health += 2
+                        print("Your max health has upgrade to " + card._max_health + "\n")
+                else:
+                    card.pop(0)
+                    print("You got too Greedy")
+
+            elif chance == 3:
+                if random_num <= 3:
+                    print("You won't do it again")
+                    if random_num <= 1:
+                        card._power += 1
+                        print("Your power has upgrade to " + card._power + "\n")
+                    else:
+                        card._max_health += 2
+                        print("Your max health has upgrade to " + card._max_health + "\n")
+                else:
+                    card.pop(0)
+                    print("Finally")
             
-
-
-
-
-        
-        
-
-
-
-        
-
-
-
-
-
-
-
-    
-
-    
-
-
-    
-
+            elif chance == 1:
+                if random_num == 1:
+                    print("Congrants you did it!!")
+                    if random_num <= 1:
+                        card._power += 1
+                        card._max_health += 2
+                        print("Your power has upgrade to " + card._power + "\n")
+                        print("Your max health has upgrade to " + card._max_health + "\n")
+                else:
+                    card.pop(0)
+                    print("You fail me") 
+                    
+            chance //= 2
+            player_choice = input(f" would you like to upgrade again? 50% change\n Y/N")
 
