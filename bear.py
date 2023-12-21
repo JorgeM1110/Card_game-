ed5ff02cf744bbcc9cb035b6b75243e177f1a7e6
Jorge <@mejia>
2023-12-21 10:36:10 -0800

    fix

--- bear.py
+++ bear.py
@@ -7,7 +7,7 @@ class Bear(card.Card):
         sigil = "None"
         super.__init__("Bear", max_hp, sigil)
 
-    def attack(self, entity, dmg):
+    def attack(self, entity):
         damage = 4
         entity.take_damage(damage)
         return self._name + " attacks a " + entity._name + " for " + str(damage) + " damage."
--- kingfisher.py
+++ kingfisher.py
@@ -7,7 +7,7 @@ class KingFisher(card.Card):
         sigil = "Waterborne & Airborne"
         super.__init__("kingFisher", max_hp, sigil)
 
-    def attack(self, entity, dmg):
+    def attack(self, entity):
         damage = 1
         entity.take_damage(damage)
         return self._name + " attacks a " + entity._name + " for " + str(damage) + " damage."
